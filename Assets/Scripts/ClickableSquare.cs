cb021ef4e2fee0638641ba783631e29345081cd5
RyanNichols98 <60655885+RyanNichols98@users.noreply.github.com>
2020-02-12 15:52:09 +0000

    Base Game Set
    Scripts
    _______
    
    Clickable Square - Game Manager
    
    Update
    ________
    
    Naughts and Crosses can be placed on a tile, basic burn system, base game layed out.

---
+++ Assets/Scripts/ClickableSquare.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableSquare : MonoBehaviour
+{
+
+    public int SquareNumber = 0;
+
+    void OnMouseDown()
+    {
+
+        GameObject.Find("Game Manager").SendMessage("SquareClicked", gameObject);
+        Destroy(this);
+
+    }
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour { 
+
+
+    // These are for Object Prefabs
+    public GameObject Naught;
+    public GameObject Cross;
+
+
+    // Turn tells us who's turn it is( 1= 0 2= x)
+    int Turn = 1;
+
+
+
+
+    public void SquareClicked(GameObject square)
+    {
+
+        //Get the square number
+        int SquareNumber = square.GetComponent<ClickableSquare>().SquareNumber;
+
+        print(SquareNumber);
+
+        SpawnPrefab(square.transform.position);
+
+        //Next player turn
+        NextTurn();
+
+    }
+
+
+
+
+
+
+    void SpawnPrefab(Vector3 postion)
+    {
+
+        // So we can see it
+        //postion.z = 0;
+        //Check who's turn it is, then sqawn their prefab
+        if (Turn == 1)
+            Instantiate(Naught, postion, Quaternion.identity);
+        else if (Turn == 2)
+            Instantiate(Cross, postion, Quaternion.identity);
+
+
+    }
+
+    void NextTurn()
+    {
+
+        //Increase Turn
+        Turn += 1;
+
+        //Check if Turn hit 3
+
+        if (Turn == 3)
+            Turn = 1;
+
+
+    }
+}
